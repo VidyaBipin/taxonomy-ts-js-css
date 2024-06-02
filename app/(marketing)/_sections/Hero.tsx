d2107fec6e8b58973351e07b5cadd325333040f3
Eyad <e.kam12@yahoo.com>
2024-06-02 18:30:03 -0400

    added flipped words

--- app/(marketing)/_sections/Hero.tsx
+++ app/(marketing)/_sections/Hero.tsx
@@ -1,15 +1,30 @@
 import { buttonVariants } from "@/components/ui/button"
+import { FlipWords } from "@/components/ui/flip-words"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import Link from "next/link"
 
 export default function HeroSection() {
+  const locations = [
+    "Washington D.C",
+    "Maryland",
+    "Virginia",
+    "Delaware",
+    "Pennsylvania",
+    "Ohio",
+    "Indiana",
+    "West Virginia",
+    "North Carolina",
+  ]
+
   return (
     <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
       <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-        <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-          Your Insurance Experts In Maryland
-        </h1>
+        <div className=" relative text-nowrap font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+          Your Insurance Experts In
+          <br /> <FlipWords words={locations} />
+        </div>
+
         <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
           Our expert team is dedicated to providing personalized service,
           ensuring peace of mind and protection for you and your loved ones.
